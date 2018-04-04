e5300d365ebd59290d03ce3ad70c2e2b45b707c4
EC2 Default User <ec2-user@ip-172-31-25-13.ap-southeast-1.compute.internal>
2018-04-04 11:22:31 +0000

    first

--- test/metadata.rb
+++
@@ -1,7 +0,0 @@
-name 'example'
-maintainer 'Kevin Dickerson'
-maintainer_email 'kevin.dickerson@loom.technology'
-license 'Apache 2.0'
-description 'Recipe cookbook that calls Chef resources defined in a separate ' \
-            'resource cookbook dependency. Integration tests are in InSpec.'
-long_description IO.read File.join(File.dirname(__FILE__), 'README.md')
